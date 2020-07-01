cca7131bb0eb4d9b26c7587248bdb399aebee34a
sumit31raj <sumit31raj@gmail.com>
2020-07-01 17:22:53 +0530

    Added progress bar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,46 +1,35 @@
 import React from 'react';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './index.css';
 
 const SideBar = () => {
   return (
     <div className="sideBar">
       <div className="sideBarInner" >
-
-        <div className="progress progress-primary" data-percentage="20">
-          <span className="progress-left">
-            <span className="progress-bar"></span>
-          </span>
-          <span className="progress-right">
-            <span className="progress-bar"></span>
-          </span>
-          <div className="progress-value">
-            <div>
-              1,000,000 
-              <span>completed</span>
-            </div>
+        <div className="progressBar">
+          <CircularProgressbarWithChildren strokeWidth={1} value={75}>
+            <p className="accuracyLabel">ACCURACY</p>
+            <p><span className="percentageLabel">75</span><span className="percentageSymbol"> %</span></p>
+          </CircularProgressbarWithChildren>
+        </div>
+        <div className="completedDiv" >
+          <div className="completedHeading" > 
+            Completed
+          </div>
+          <div className="completedNumber" > 
+            100 <span className="problemsTitle" > problems </span>
           </div>
         </div>
-
-         <div className="completedDiv" >
-           <div className="completedHeading" > 
-           Completed
-           </div>
-           <div className="completedNumber" > 
-              100 <span className="problemsTitle" > problems </span>
-           </div>
-           </div>
-
-           <div className="completedDiv" >
-           <div className="completedHeading" > 
-           Correct
-           </div>
-           <div className="completedNumber" > 
-              75 <span className="problemsTitle" > problems </span>
-           </div>
-           </div>
-
-          <div className="pastData" > past 24 hours, 2020  2/20 </div>
-
+        <div className="completedDiv" >
+          <div className="completedHeading" > 
+            Correct
+          </div>
+          <div className="completedNumber" > 
+            75 <span className="problemsTitle" > problems </span>
+          </div>
+        </div>
+        <div className="pastData" > past 24 hours, 2020  2/20 </div>
       </div>
     </div>
   );
