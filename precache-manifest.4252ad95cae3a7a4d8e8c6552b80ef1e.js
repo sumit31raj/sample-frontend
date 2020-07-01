9a4baf0c7c354ab27e1dc9733562977a1185f403
sumit31raj <sumit31raj@gmail.com>
2020-07-01 15:30:51 +0530

    Updates

---
+++ precache-manifest.4252ad95cae3a7a4d8e8c6552b80ef1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e687bc5e9108ed72d5504273bca4514e",
+    "url": "/sample-frontend/index.html"
+  },
+  {
+    "revision": "0721b3a11fd22461f7ed",
+    "url": "/sample-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1634452a10223e781626",
+    "url": "/sample-frontend/static/js/2.c175df61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-frontend/static/js/2.c175df61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0721b3a11fd22461f7ed",
+    "url": "/sample-frontend/static/js/main.5f219e5c.chunk.js"
+  },
+  {
+    "revision": "6c3f3834e65513aafa29",
+    "url": "/sample-frontend/static/js/runtime-main.c86cb665.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
