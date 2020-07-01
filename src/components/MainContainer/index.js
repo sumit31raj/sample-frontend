eeb5e1ee34da809673224c47c21835a24c2fa5b0
sumit31raj <sumit31raj@gmail.com>
2020-07-01 15:43:17 +0530

    Added basic layout

---
+++ src/components/MainContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const MainContainer = () => {
+  return (
+    <div className="mainContainer">
+      MainContainer
+    </div>
+  );
+}
+
+export default MainContainer;
---
+++ src/components/SideBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const SideBar = () => {
+  return (
+    <div className="sideBar">
+      SideBar
+    </div>
+  );
+}
+
+export default SideBar;
